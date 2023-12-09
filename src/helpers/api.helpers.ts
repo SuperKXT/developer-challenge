0324d2711c83c6770785a6274a11641ce53367f3
SuperKXT <superkxt@outlook.com>
2023-12-09 16:48:08 +0500

    Overhaul `helpers/api`

--- src/helpers/api.helpers.ts
+++ src/helpers/api.helpers.ts
@@ -1,31 +1,31 @@
-import { z } from 'zod';
+import { http } from 'msw';
 
-import { backendPath } from '../config.js';
 import { ApiError, AuthError, ConnectionError } from '../errors.js';
+import { handlers } from '../mocks/handlers.js';
+import { apiResponseSchema } from '../schemas.js';
 
-const responseSchema = z.object({
-	statusCode: z.number(),
-	error: z.string(),
-	message: z.string(),
-	data: z.unknown().optional(),
-});
+import type { ResponseResolver } from 'msw';
+import type { z } from 'zod';
+import type { Utils } from '../types/utils.types.js';
+
+export type ApiMethod = 'get' | 'patch' | 'put' | 'post' | 'delete';
 
 /**
  * the helper to send a request to the backend
  * @param apiPath the path of the api request
- * @param method the method of the current request. defaults to `GET`
+ * @param method the method of the current request. defaults to `get`
  * @param body the body to sen with the request
  */
 const apiRequest = async <Response = unknown>(
 	apiPath: string,
-	method: 'GET' | 'PATCH' | 'PUT' | 'POST' | 'DELETE',
+	method: ApiMethod,
 	body?: Obj | Obj[] | FormData,
 ): Promise<Response> => {
 	if (!navigator.onLine)
 		throw new ConnectionError('not connected to the internet!');
 
 	const options: Omit<RequestInit, 'headers'> & { headers: Headers } = {
-		method,
+		method: method.toUpperCase(),
 		headers: new Headers(),
 	};
 
@@ -36,74 +36,66 @@ const apiRequest = async <Response = unknown>(
 		options.body = body;
 	}
 
-	const response = await fetch(`${backendPath}/${apiPath}`, options);
+	const response = await fetch(apiPath, options);
 	if (response.status === 401) throw new AuthError('login expired!');
 
-	const result = responseSchema.safeParse(await response.json());
-	if (!result.success) throw new ApiError('invalid api response format!');
-	const { statusCode, message, data } = result.data;
-	if (statusCode !== 200) throw new ApiError(message);
+	const result = apiResponseSchema.safeParse(await response.json());
+	if (!result.success) throw new ApiError('invalid api response  format!');
+	if (result.data.error) throw new ApiError(result.data.message);
 	else if (!response.ok) throw new ApiError('Unknown api error');
-	return data as Response;
+	return result.data.data as never;
 };
 
-/**
- * the helper to send a `GET` request to the backend
- * @param apiPath the path of the api request
- * @param options the options to send with the request
- * @param options.schema the zod schema to parse the response with
- */
-export const getRequest = async <Schema extends z.ZodSchema = z.ZodUnknown>(
-	apiPath: string,
-	options?: {
-		schema?: Schema;
-	},
-): Promise<Schema['_output']> => {
-	const response = apiRequest(apiPath, 'GET');
-	if (!options?.schema) return response;
-	return response.then(async (data) => {
-		const parsed = (
-			options.schema ? options.schema.parse(data) : data
-		) as Promise<Schema['_output']>;
-		return parsed;
-	});
+type ParamsFromPath<
+	str extends string,
+	params extends string = never,
+> = str extends `${string}:${infer param}/${infer rest}`
+	? ParamsFromPath<rest, params | param>
+	: str extends `${string}:${infer param}`
+	  ? params | param
+	  : params;
+
+type _ = [{}] extends [Record<string, never>] ? 1 : 0;
+//   ^?
+
+export const createRoute = <
+	Path extends string,
+	Body extends z.AnyZodObject | z.ZodArray<z.ZodUnknown> = never,
+	Response extends
+		| z.AnyZodObject
+		| z.ZodString
+		| z.ZodArray<z.AnyZodObject> = never,
+	Params extends string = ParamsFromPath<Path>,
+>(route: {
+	path: Path;
+	method: ApiMethod;
+	body?: Body;
+	response?: Response;
+	mock: ResponseResolver<
+		{ params: { [k in Params]: string } },
+		Body['_output'],
+		Response['_output']
+	>;
+}): ((
+	...args: ([Params] extends [never]
+		? unknown
+		: { params: { [k in Params]: string } }) &
+		([Body] extends [never] ? {} : { body: Body['_output'] }) extends infer Args
+		? Args extends Record<string, never>
+			? []
+			: [args: Utils.prettify<Args>]
+		: [never]
+) => Promise<Response['_output']>) => {
+	handlers.push(http[route.method](route.path, route.mock));
+	return (async (args: {
+		params: Record<string, string> | undefined;
+		body: unknown;
+	}) => {
+		const path = args.params
+			? route.path.replace(/:[^/]+)/gu, (match) =>
+					args.params ? (args.params[match.slice(1)] as string) : match,
+			  )
+			: route.path;
+		return apiRequest(path, route.method, args.body as never);
+	}) as never;
 };
-
-/**
- * the helper to send a `PATCH` request to the backend
- * @param apiPath the path of the api request
- * @param body the body of the request
- */
-export const patchRequest = async <Response = unknown>(
-	apiPath: string,
-	body: Obj | FormData,
-) => apiRequest<Response>(apiPath, 'PATCH', body);
-
-/**
- * the helper to send a `PUT` request to the backend
- * @param apiPath the path of the api request
- * @param body the body of the request
- */
-export const putRequest = async <Response = unknown>(
-	apiPath: string,
-	body: Obj | FormData,
-) => apiRequest<Response>(apiPath, 'PUT', body);
-
-/**
- * the helper to send a `POST` request to the backend
- * @param apiPath the path of the api request
- * @param body the body of the request
- */
-export const postRequest = async <Response = unknown>(
-	apiPath: string,
-	body: Obj | Obj[] | FormData,
-) => apiRequest<Response>(apiPath, 'POST', body);
-
-/**
- * the helper to send a `DELETE` request to the backend
- * @param apiPath the path of the api request
- */
-export const deleteRequest = async <Response = unknown>(
-	apiPath: string,
-	body?: Obj | Obj[] | FormData,
-) => apiRequest<Response>(apiPath, 'DELETE', body);
